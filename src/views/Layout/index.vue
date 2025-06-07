5baf81ab4cd55244f4e6d4117b11b9d149e8e228
Fairy0724 <2664195548@qq.com>
2025-06-07 20:33:17 +0800

    静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,14 +1,23 @@
 <script setup>
-defineOptions({
-  name: "LayoutIndex",
-});
+defineOptions({ name: 'LayoutIndex' })
+
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHome from './components/LayoutHome.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+
 </script>
 
 <template>
   <div>
-    <h3>我是首页</h3>
+    <!-- 顶部导航栏 -->
+    <LayoutNav />
+    <!-- 头部 -->
+    <LayoutHome />
+    <!-- 内容区 -->
     <!-- 二级路由出口 -->
     <RouterView />
+    <!-- 底部 -->
+    <LayoutFooter />
   </div>
 </template>
 
