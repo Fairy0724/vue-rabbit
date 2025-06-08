6f907e2a5a56fc4fa03a0e4c1f6802859512661f
Fairy0724 <2664195548@qq.com>
2025-06-08 15:56:56 +0800

    Home新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,8 +1,20 @@
 import httpInstance from '@/utils/http'
+import { ElDescriptions } from 'element-plus'
 
 // 获取bannner
 export function getBannerAPI() {
   return httpInstance({
     url:'/home/banner'
+  })
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
   })
 }
