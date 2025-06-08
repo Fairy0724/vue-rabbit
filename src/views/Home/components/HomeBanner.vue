dfa5aa07e828f9d165a5e0ab9decfd7f83e4c530
Fairy0724 <2664195548@qq.com>
2025-06-08 14:41:09 +0800

    home整体架构拆分与分类渲染

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  我是Banner
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,22 @@
 <script setup>
 defineOptions({
-  name: 'HomeIndex'
-})
+  name: "HomeIndex",
+});
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeProduct from "./components/HomeProduct.vue";
 </script>
 
 <template>
-  <div style="height: 1200px;">
-	<h3>我是Home页</h3>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
   </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
