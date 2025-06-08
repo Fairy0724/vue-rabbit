14fc0fb1b4ffef588615258350569840cc42e4cb
Fairy0724 <2664195548@qq.com>
2025-06-08 15:47:07 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -7,6 +7,7 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue"
 </script>
 
 <template>
@@ -17,6 +18,16 @@ import HomeProduct from "./components/HomeProduct.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+    <div>
+      插槽
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+
+</HomePanel>
 </template>
 
 <style scoped></style>
