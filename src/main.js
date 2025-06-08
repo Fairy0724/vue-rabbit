71d541ed427564ab4078cb7eb932f6f94a2d7de8
Fairy0724 <2664195548@qq.com>
2025-06-08 16:31:37 +0800

    Home图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -10,11 +10,30 @@ import '@/styles/common.scss'
 // import {getCategoryAPI} from './apis/testAPI'
 // getCategoryAPI().then(res => {
 //   console.log(res);
-  
 // })
+
+import { useIntersectionObserver } from '@vueuse/core'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el---指令绑定的元素 img
+    // binding---binding.value指令等号后绑定表达式的值 图片url
+    const { stop } = useIntersectionObserver(
+      el,//监听谁
+      ([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+          stop()
+        }
+    }
+    )
+  }
+})
