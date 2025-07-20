70cd9298913d08bc3f25a7244882642ca714e4e9
Fairy0724 <2664195548@qq.com>
2025-07-20 13:53:24 +0800

    '详情页-热榜'

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -18,7 +18,7 @@ onMounted(() => {
     <!-- 插槽 -->
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img v-img-lazy="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">¥{{ item.price }}</p>
