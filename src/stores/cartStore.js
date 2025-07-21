67fe85f8daac78bf0299ca3dfefa6881ea148399
Fairy0724 <2664195548@qq.com>
2025-07-21 20:32:46 +0800

    本地购物车-列表购物车单选全选

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -34,12 +34,37 @@ export const useCartStore = defineStore('cart', () => {
   const allCount = computed(()=>cartList.value.reduce((total, item) => total + item.count, 0))
   // 计算总价
   const allPrice = computed(() => cartList.value.reduce((price, item) => price + item.count * item.price, 0))
+  
+  // 单选框
+  const singleCheck = (skuId, selected) => {
+    // skuId是商品的唯一标识，selected是选中状态
+    // 找到对应的商品对象，修改selected状态
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    if (item) {
+      item.selected = selected
+    }
+  }
+  // 全选
+  const allCheck = (selected) => {
+    // selected是全选框的选中状态
+    cartList.value.forEach(item => {
+      item.selected = selected
+    })
+  }
+  //是否全选
+  const isAll = computed(() => 
+    // 判断cartList中所有商品的selected状态是否都为true
+    cartList.value.every(item => item.selected)
+)
   return {
     cartList,
     addCart,
     delCart,
     allCount, 
     allPrice,
+    singleCheck,
+    allCheck,
+    isAll
   }
 }, {
   persist: true,
